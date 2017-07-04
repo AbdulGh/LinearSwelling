e60282d18829ec80528073544254d663d1bd60a7
Abdul <abdul.ghani@dur.ac.uk>
2017-07-04 06:27:44 +0100

    I don't know how tk works

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import calibration.C
+import calibration
 import measure
 
 from tkinter import *
@@ -6,60 +6,51 @@ from tkinter import messagebox
 from tkinter import filedialog
 from tkinter.ttk import *
 
-class MainWindow(Frame):
+class MainWindow(Tk):
     def __init__(self):
-        Frame.__init__(self)
-        self.master = Tk()
-        self.master.title("Swellometer")
-        #self.master.resizable(False, False)
+        Tk.__init__(self)
+        self.title("Swellometer")
+        self.minsize(200,200)
+        self.resizable(False, False)
 
         self.initwindow()
-		
-		self.master.mainloop()
-		
-	def initwindow(self):
-		Label(self.master, text="Swellometer", font="Helvetica 16").pack()
 
-		def calibrationOption():
-			root = Tk()
-			calibrationWindow = calibration.CalibrationWindow(root)
-			root.mainloop()
+        self.mainloop()
 
-			m,b = calibrationWindow.getsettings()
+    def initwindow(self):
+        Label(self, text="Swellometer", font="Helvetica 16").pack(pady=10)
 
-			root = Tk()
-			calibrationWindow = measure.ExperimentWindow(root, m, b)
-			root.mainloop()
-			root.grab_set()
-            root.wa
+        def calibrationOption():
+            calibrationWindow = calibration.CalibrationWindow(self)
+            m,b = calibrationWindow.getsettings()
 
-		launchCalibration = Button(self.master, text="Calibrate sensors", command=calibrationOption)
-		launchCalibration.pack()
+            calibrationWindow = calibration.CalibrationWindow(self)
 
-		def loadSettingsOption():
-			f = filedialog.askopenfilename()
-			m = b = None
-			if f is not None:
-				try:
-					m = f.readline()
-					b = f.readline()
-					m = float(m)
-					b = float(b)
-				except ValueError:
-					messagebox.showerror("Invalid file", "Could not read line from this file")
+        launchCalibration = Button(self, text="Calibrate sensors", command=calibrationOption)
+        launchCalibration.pack(pady=10)
 
-			if m is not None:
-				root = Tk()
-				calibrationWindow = calibration.ExperimentWindow(root)
-				root.mainloop()
-				wait_window(root)
+        def loadSettingsOption():
+            f = filedialog.askopenfilename()
+            m = b = None
+            if f is not None:
+                try:
+                    m = f.readline()
+                    b = f.readline()
+                    m = float(m)
+                    b = float(b)
+                except ValueError:
+                    messagebox.showerror("Invalid file", "Could not read line from this file")
+
+            if m is not None:
+                root = Tk()
+                calibrationWindow = measure.ExperimentWindow(root)
+
+        loadCalibration = Button(self, text="Load calibration", command=loadSettingsOption)
+        loadCalibration.pack(pady=10)
+
+        Button(self, text="Close", command=exit).pack(pady=10)
+
+if __name__ == '__main__':
+    MainWindow()
 
 
-		loadCalibration = Button(self.master, text="Load calibration", command=loadSettingsOption)
-		loadCalibration.pack()
-
-		Button(self.master, text="Close", command=exit).pack()
-				
-			
-			
-	
